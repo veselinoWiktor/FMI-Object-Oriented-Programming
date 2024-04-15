fb7a6ab744b32cb6827b81808acd90ae299fee35
veselinoWiktor <vveselinov11@gmail.com>
2024-04-16 01:05:19 +0300

    Exercise01 add further declaration of possible functions

--- Homeworks/Homework02/Exercise01/MultiSet.h
+++ Homeworks/Homework02/Exercise01/MultiSet.h
@@ -54,7 +54,18 @@ public:
 	friend MultiSet Intersect(const MultiSet& lhs, const MultiSet& rhs);
 	friend MultiSet Differnce(const MultiSet& lhs, const MultiSet& rhs);
 	MultiSet Complement() const;
+
+	//Implement if enough time
+	//MultiSet& operator&=(const MultiSet& other); //Intersection
+	//MultiSet& operator|=(const MultiSet& other); //Union
+	//MultiSet& operator/=(const MultiSet& other); //Difference
 };
+
+//Implement if enough time
+//MultiSet operator&(const MultiSet& lhs, const MultiSet& rhs); //Intersection
+//MultiSet operator|(const MultiSet& lhs, const MultiSet& rhs); //Union
+//MultiSet operator/(const MultiSet& lhs, const MultiSet& rhs); //Difference
+//MultiSet operator~(const MultiSet& other); //Complement
 
 unsigned powerOfTwo(unsigned n);
 unsigned getFileSize(std::ifstream& ifs);

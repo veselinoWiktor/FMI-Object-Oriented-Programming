213b28e4b5cbd43fad33b319a21e2912a24db375
Viktor Veselinov <80973087+veselinoWiktor@users.noreply.github.com>
2024-05-22 21:41:45 +0300

    Update String.h

--- Practicums/Week09/StringVector/String.h
+++ Practicums/Week09/StringVector/String.h
@@ -1,48 +1,41 @@
 #pragma once
-#include <iostream>
+#include <fstream>
 
 class String
 {
-public:
-    String();
-    String(const char* data);
-
-    String(const String& other);
-    String(String&& other);
-
-    String& operator=(const String& other);
-
-    String& operator=(String&& other);
-    ~String();
-
-    size_t getCapacity() const;
-    size_t getSize() const;
-
-    const char* c_str() const;
-
-    String& operator+=(const String& other);
-
-    char& operator[](size_t index);
-    const char& operator[](size_t index) const;
-
-    friend std::ostream& operator<<(std::ostream& os, const String& obj);
-    friend std::istream& operator>>(std::istream& is, String& ref);
-    friend String operator+(const String& lhs, const String& rhs);
 private:
-    explicit String(size_t stringLength);
-    void resize(unsigned newAllocatedDataSize);
+	char* data;
+	size_t length;
+	size_t capacity;
 
-    void free();
-    void copyFrom(const String& other);
+	void free();
+	void copyFrom(const String& other);
+	void moveFrom(String&& other);
 
-    void moveFrom(String&& other);
+	void resize(size_t newCapacity);
 
-    char* _data;
-    size_t _size;
-    size_t _allocatedDataSize;
+	explicit String(size_t size);
+public:
+	String();
+	String(const char* str);
+	String(const String& other);
+	String(String&& other) noexcept;
+	String& operator=(const String& other);
+	String& operator=(String&& other) noexcept;
+	~String();
+
+	size_t getLength() const;
+	const char* c_str() const;
+
+	String& operator+=(const String& other);
+
+	char& operator[](size_t idx);
+	const char& operator[](size_t idx) const;
+
+	friend std::ostream& operator<<(std::ostream& os, const String& obj);
+	friend std::istream& operator>>(std::istream& is, String& obj);
+	friend String operator+(const String& lhs, const String& rhs);
 };
-
-
 
 bool operator==(const String& lhs, const String& rhs);
 bool operator!=(const String& lhs, const String& rhs);

b6e00d97ce1102d895ec9464214bcf5e6bff80ea
veselinoWiktor <vveselinov11@gmail.com>
2024-05-28 20:28:10 +0300

    Implement MaximalPartialFunction class

--- Homeworks/Homework03/PartialFunction/MaximalPartialFunction.cpp
+++ Homeworks/Homework03/PartialFunction/MaximalPartialFunction.cpp
@@ -1,1 +1,50 @@
 #include "MaximalPartialFunction.h"
+
+MaximalPartialFunction::MaximalPartialFunction(const Container<PartialFunction>& functions) : functions(functions)
+{
+}
+
+int MaximalPartialFunction::getFunctionValue(int x) const
+{
+	if (!isFunctionDefinedIn(x))
+	{
+		return 0;
+	}
+
+	int32_t maxValue = INT32_MIN;
+	size_t size = functions.getSize();
+	int32_t currFunctionValue = 0;
+	for (size_t i = 0; i < size; i++)
+	{
+		currFunctionValue = functions[i]->getFunctionValue(x);
+		if (currFunctionValue > maxValue)
+		{
+			maxValue = currFunctionValue;
+		}
+	}
+
+	return maxValue;
+}
+
+bool MaximalPartialFunction::isFunctionDefinedIn(int x) const
+{
+	size_t size = functions.getSize();
+	for (size_t i = 0; i < size; i++)
+	{
+		if (!functions[i]->isFunctionDefinedIn(x))
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+PartialFunction* MaximalPartialFunction::clone() const
+{
+	return new MaximalPartialFunction(*this);
+}
+
+int MaximalPartialFunction::operator()(int x) const
+{
+	return getFunctionValue(x);
+}
--- Homeworks/Homework03/PartialFunction/MaximalPartialFunction.h
+++ Homeworks/Homework03/PartialFunction/MaximalPartialFunction.h
@@ -1,7 +1,16 @@
 #pragma once
 #include "PartialFunction.h"
+#include "Container.hpp"
 
 class MaximalPartialFunction : public PartialFunction
 {
+	Container<PartialFunction> functions;
+
+public:
+	MaximalPartialFunction(const Container<PartialFunction>& functions);
+	int getFunctionValue(int x) const override;
+	bool isFunctionDefinedIn(int x) const override;
+	PartialFunction* clone() const override;
+	int operator()(int x) const;
 };
 

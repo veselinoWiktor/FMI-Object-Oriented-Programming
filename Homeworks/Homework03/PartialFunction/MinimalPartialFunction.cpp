b142e28980a2c582fc4228a01f59a43f6912a5bf
veselinoWiktor <vveselinov11@gmail.com>
2024-05-28 20:28:27 +0300

    Implement MinimalPartialFunction class

--- Homeworks/Homework03/PartialFunction/MinimalPartialFunction.cpp
+++ Homeworks/Homework03/PartialFunction/MinimalPartialFunction.cpp
@@ -1,1 +1,50 @@
 #include "MinimalPartialFunction.h"
+
+MinimalPartialFunction::MinimalPartialFunction(const Container<PartialFunction>& functions) : functions(functions)
+{
+}
+
+int MinimalPartialFunction::getFunctionValue(int x) const
+{
+	if (!isFunctionDefinedIn(x))
+	{
+		return 0;
+	}
+
+	int32_t minValue = INT32_MAX;
+	size_t size = functions.getSize();
+	int32_t currFunctionValue = 0;
+	for (size_t i = 0; i < size; i++)
+	{
+		currFunctionValue = functions[i]->getFunctionValue(x);
+		if (currFunctionValue < minValue)
+		{
+			minValue = currFunctionValue;
+		}
+	}
+
+	return minValue;
+}
+
+bool MinimalPartialFunction::isFunctionDefinedIn(int x) const
+{
+	size_t size = functions.getSize();
+	for (size_t i = 0; i < size; i++)
+	{
+		if (!functions[i]->isFunctionDefinedIn(x))
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+PartialFunction* MinimalPartialFunction::clone() const
+{
+	return new MinimalPartialFunction(*this);
+}
+
+int MinimalPartialFunction::operator()(int x) const
+{
+	return getFunctionValue(x);
+}
--- Homeworks/Homework03/PartialFunction/MinimalPartialFunction.h
+++ Homeworks/Homework03/PartialFunction/MinimalPartialFunction.h
@@ -1,7 +1,16 @@
 #pragma once
 #include "PartialFunction.h"
+#include "Container.hpp"
 
 class MinimalPartialFunction : public PartialFunction
 {
+	Container<PartialFunction> functions;
+
+public:
+	MinimalPartialFunction(const Container<PartialFunction>& functions);
+	int getFunctionValue(int x) const override;
+	bool isFunctionDefinedIn(int x) const override;
+	PartialFunction* clone() const override;
+	int operator()(int x) const;
 };
 

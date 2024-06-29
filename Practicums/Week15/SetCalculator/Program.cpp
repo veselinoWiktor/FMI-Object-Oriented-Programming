7e6150137067773ee6865cea9869f2909347186c
veselinoWiktor <vveselinov11@gmail.com>
2024-06-29 17:40:54 +0300

    update

--- Practicums/Week15/SetCalculator/Program.cpp
+++ Practicums/Week15/SetCalculator/Program.cpp
@@ -1,16 +1,5 @@
 #include <iostream>
-#include "Queue.hpp"
 
 int main()
 {
-	Queue<int> queue;
-
-	queue.push(1);
-	queue.push(2);
-	queue.push(3);
-	queue.push(4);
-
-	queue.pop();
-	queue.push(5);
-	queue.push(6);
 }
